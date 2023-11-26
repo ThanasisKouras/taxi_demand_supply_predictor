8e2d97905439b5fa082d88eb4ec39755af08d5b9
Athanasios Kouras <88839122+ThanasisKouras@users.noreply.github.com>
2023-11-26 17:56:55 +0200

    Update feature_pipeline.py

--- scripts/feature_pipeline.py
+++ scripts/feature_pipeline.py
@@ -1,6 +1,9 @@
-# Add the project root to the Python path
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, project_root)
+import sys
+import os
+
+# Add the 'src' directory to the Python path
+src_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "src")
+sys.path.insert(0, src_path)
 
 from datetime import datetime, timedelta
 from argparse import ArgumentParser
