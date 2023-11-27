acc6e93974cbaa893218511007c54b6c7ee98a7c
Athanasios Kouras <88839122+ThanasisKouras@users.noreply.github.com>
2023-11-27 16:06:42 +0200

    Update feature_pipeline.py

--- scripts/feature_pipeline.py
+++ scripts/feature_pipeline.py
@@ -2,7 +2,8 @@ import sys
 import os
 
 # Add the 'src' directory to the Python path
-src_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "src")
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 #sys.path.insert(0, src_path)
 
 from datetime import datetime, timedelta
