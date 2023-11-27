14084b0812f1883eb2579831d8874029206e273d
Athanasios Kouras <tkour@outlook.com>
2023-11-27 22:45:47 +0200

    change to utc

--- scripts/feature_pipeline.py
+++ scripts/feature_pipeline.py
@@ -8,7 +8,7 @@ from argparse import ArgumentParser
 from pdb import set_trace as stop
 
 import pandas as pd
-
+import numpy as np
 from src import config
 from src.data import (
     fetch_ride_events_from_data_warehouse,
@@ -48,7 +48,7 @@ def run(date: datetime):
     # add new column with the timestamp in Unix seconds
     logger.info('Adding column `pickup_ts` with Unix seconds...')
     ts_data['pickup_hour'] = pd.to_datetime(ts_data['pickup_hour'], utc=True)
-    ts_data['pickup_ts'] = ts_data['pickup_hour'].astype(int) // 10 ** 6
+    ts_data['pickup_ts'] = (ts_data['pickup_hour'].astype('int64') // 10**6).astype('int32')
 
     # get a pointer to the feature group we wanna write to
     logger.info('Getting pointer to the feature group we wanna save data to')
