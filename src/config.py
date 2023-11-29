ebbdc4e2036b938854a1f532664ab136d1f3a04d
Athanasios Kouras <tkour@outlook.com>
2023-11-29 14:43:22 +0200

    update config, data_split

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ from src.feature_store_api import FeatureGroupConfig, FeatureViewConfig
 # load key-value pairs from .env file located in the parent directory
 load_dotenv(PARENT_DIR / '.env')
 
-HOPSWORKS_PROJECT_NAME = 'taxi_demand_supply'
+HOPSWORKS_PROJECT_NAME = 'taxi_demand'
 try:
     # HOPSWORKS_PROJECT_NAME = os.environ['HOPSWORKS_PROJECT_NAME']
     HOPSWORKS_API_KEY = os.environ['HOPSWORKS_API_KEY']
@@ -16,10 +16,10 @@ except:
 
 # TODO: remove FEATURE_GROUP_NAME and FEATURE_GROUP_VERSION, and use FEATURE_GROUP_METADATA instead
 FEATURE_GROUP_NAME = 'time_series_hourly_feature_group'
-FEATURE_GROUP_VERSION = 4
+FEATURE_GROUP_VERSION = 3
 FEATURE_GROUP_METADATA = FeatureGroupConfig(
     name='time_series_hourly_feature_group',
-    version=4,
+    version=3,
     description='Feature group with hourly time-series data of historical taxi rides',
     primary_key=['pickup_location_id', 'pickup_ts'],
     event_time='pickup_ts',
@@ -28,10 +28,10 @@ FEATURE_GROUP_METADATA = FeatureGroupConfig(
 
 # TODO: remove FEATURE_VIEW_NAME and FEATURE_VIEW_VERSION, and use FEATURE_VIEW_METADATA instead
 FEATURE_VIEW_NAME = 'time_series_hourly_feature_view'
-FEATURE_VIEW_VERSION = 4
+FEATURE_VIEW_VERSION = 3
 FEATURE_VIEW_METADATA = FeatureViewConfig(
     name='time_series_hourly_feature_view',
-    version=4,
+    version=3,
     feature_group=FEATURE_GROUP_METADATA,
 )
 
@@ -52,12 +52,12 @@ FEATURE_GROUP_PREDICTIONS_METADATA = FeatureGroupConfig(
 FEATURE_VIEW_MODEL_PREDICTIONS = 'model_predictions_feature_view'
 FEATURE_VIEW_PREDICTIONS_METADATA = FeatureViewConfig(
     name='model_predictions_feature_view',
-    version=3,
+    version=4,
     feature_group=FEATURE_GROUP_PREDICTIONS_METADATA,
 )
 
 MONITORING_FV_NAME = 'monitoring_feature_view'
-MONITORING_FV_VERSION = 2
+MONITORING_FV_VERSION = 3
 
 # number of historical values our model needs to generate predictions
 N_FEATURES = 24 * 28
@@ -67,5 +67,3 @@ N_HYPERPARAMETER_SEARCH_TRIALS = 1
 
 # maximum Mean Absolute Error we allow our production model to have
 MAX_MAE = 7.0
-
-print("PARENT_DIR:", PARENT_DIR)
--- src/data_split.py
+++ src/data_split.py
@@ -13,12 +13,20 @@ def train_test_split(
     # Ensure cutoff_date is timezone-aware (UTC)
     cutoff_date = cutoff_date.replace(tzinfo=timezone.utc)
 
+
+    # Convert pickup_hour to pandas datetime with timezone--------
+    df['pickup_hour'] = pd.to_datetime(df['pickup_hour'], utc=True)
+
     ## Convert pickup_hour to pandas datetime with timezone##
     df['pickup_hour'] = pd.to_datetime(df['pickup_hour'], utc=True)
 
     # Assuming the pickup_hour column is already timezone-aware
     train_data = df[df.pickup_hour < cutoff_date].reset_index(drop=True)
     test_data = df[df.pickup_hour >= cutoff_date].reset_index(drop=True)
+
+    # Print the number of samples before and after the cutoff date
+    print(f'Number of samples before cutoff date: {len(train_data)}')
+    print(f'Number of samples after cutoff date: {len(test_data)}')
 
     X_train = train_data.drop(columns=[target_column_name])
     y_train = train_data[target_column_name]
